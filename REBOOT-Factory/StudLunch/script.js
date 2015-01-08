8d19fb5409a56c0e0ea3a877d6c7721206a3ac0e
Antony Paquay <antony_p@outlook.com>
2015-01-08 21:29:01 +0100

    Hey hey we process !!!:)

--- REBOOT-Factory/StudLunch/script.js
+++ REBOOT-Factory/StudLunch/script.js
@@ -1,16 +1,19 @@
 
 var wheel1 = document.getElementsByClassName("wheel1"),
-	section__list = document.getElementsByClassName("section__list");
+	section__list = document.getElementsByClassName("section__list"),
+	listAnimation =document.getElementsByClassName("listAnimation");
 
 		wheel1[0].addEventListener("click", function(){
-		section__list[0].classList.add("section__list--list");
-		section__list[0].classList.remove("section__list--map")
+		section__list[0].classList.add("section__list--list"),
+		section__list[0].classList.remove("section__list--map"),
+		listAnimation[0].classList.add("listAnimation--go");
 	});
 
 var wheel2 =document.getElementsByClassName("wheel2"),
 	section__list =document.getElementsByClassName("section__list");
 
 		wheel2[0].addEventListener("click", function(){
-		section__list[0].classList.add("section__list--map")
+		section__list[0].classList.add("section__list--map"),
 		section__list[0].classList.remove("section__list--list");
+		
 	});
